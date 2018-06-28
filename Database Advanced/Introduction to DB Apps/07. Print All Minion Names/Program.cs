40c2083570e80f439209ae0e5ece2b6990f5aee2
RosenBobchev <rosenbobchev@abv.bg>
2018-06-28 16:04:32 +0300

    update

---
+++ Database Advanced/Introduction to DB Apps/07. Print All Minion Names/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace _07._Print_All_Minion_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Server=RNB\SQLEXPRESS;" + "Integrated Security=true;" + "initial catalog=MinionsDB";
+
+            SqlConnection dbCon = new SqlConnection(connectionString);
+            dbCon.Open();
+
+            List<string> minionsInitial = new List<string>();
+            List<string> minionsArranged = new List<string>();
+
+            using (dbCon)
+            {
+                SqlCommand command = new SqlCommand("SELECT Name FROM Minions", dbCon);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                using (reader)
+                {
+                    if (!reader.HasRows)
+                    {
+                        reader.Close();
+                        dbCon.Close();
+                        return;
+                    }
+
+                    while (reader.Read())
+                    {
+                        minionsInitial.Add((string)reader["Name"]);
+                    }
+                }
+            }
+
+            while (minionsInitial.Count > 0)
+            {
+                minionsArranged.Add(minionsInitial[0]);
+                minionsInitial.RemoveAt(0);
+
+                if (minionsInitial.Count > 0)
+                {
+                    minionsArranged.Add(minionsInitial[minionsInitial.Count - 1]);
+                    minionsInitial.RemoveAt(minionsInitial.Count - 1);
+                }
+            }
+
+            minionsArranged.ForEach(m => Console.WriteLine(m));
+        }
+    }
+}
---
+++ Database Advanced/Introduction to DB Apps/09. Increase Age Stored Proc/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _09._Increase_Age_Stored_Proc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Server=RNB\SQLEXPRESS;" + "Integrated Security=true;" + "initial catalog=MinionsDB";
+
+            SqlConnection dbCon = new SqlConnection(connectionString);
+            dbCon.Open();
+
+            int id = int.Parse(Console.ReadLine());
+
+            using (dbCon)
+            {
+                var command = new SqlCommand("EXEC usp_GetOlder @Id", dbCon);
+                command.Parameters.AddWithValue("@Id", id);
+
+                command.ExecuteNonQuery();
+
+                command = new SqlCommand("SELECT * FROM Minions WHERE Id = @Id", dbCon);
+                command.Parameters.AddWithValue("@Id", id);
+
+                var reader = command.ExecuteReader();
+
+                using (reader)
+                {
+                    reader.Read();
+
+                    Console.WriteLine($"{(string)reader["Name"]} - {(int)reader["Age"]} years old");
+                }
+            }
+        }
+    }
+}
